5117a34a2b0f20a94e535f6843d2b96e67a204ee
外間美希 <hokamiki@sotomamiki-no-MacBook-Air.local>
2018-02-18 22:07:25 +0900

    進む、戻るボタン機能実装

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -12,42 +12,64 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBAction func onNext(_ sender: Any) {
+        // 1つ進む
+        dispImageNo += 1
+        
+        displayImage()
+    }
+    
+    @IBAction func onReturn(_ sender: Any) {
+        
+        dispImageNo -= 1
+        
+        displayImage()
+    }
+    
     // セグエを使用して画面遷移 segueID"result"
     // performSegueWithIdentifier("result", sender: nil)
     
+    // 表示している画像の番号
+    var dispImageNo = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // 表示している画像の番号
-        var dispImageNo = 0
+        displayImage()
         
-        func displayImage() {
-            
-            // 画像の配列
-            let imageNameArray = [
-                "1.jpg",
-                "2.jpg",
-                "3.jpg",
-                ]
-            
-            // 表示している画像の番号から名前を取り出す
-            let name = imageNameArray[dispImageNo]
-            
-            // 画像を読み込み
-            let image = UIImage(named: name)
-            
-            // ImageViewに読み込んだ画像をセット
-            imageView.image = image
+    }
+    
+    func displayImage() {
+        
+        // 画像の配列
+        let imageNameArray = [
+            "1.jpg",
+            "2.jpg",
+            "3.jpg",
+            ]
+        
+        if dispImageNo < 0 {
+            dispImageNo = 2
         }
+        
+        if dispImageNo > 2 {
+            dispImageNo = 0
+        }
+        
+        // 表示している画像の番号から名前を取り出す
+        let name = imageNameArray[dispImageNo]
+        
+        // 画像を読み込み
+        let image = UIImage(named: name)
+        
+        // ImageViewに読み込んだ画像をセット
+        imageView.image = image
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
